159314167270c75990de898c2420fccd088b09ea
MariaJoseDiaz <mjdiaz9@uc.cl>
2017-02-14 22:13:28 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,32 +1,3 @@
-var bgcolor;
-
-  var x = 100;
-  var y = 100;
-  var rect_widht = 200;
-  var rect_height = 150;
-  
-function setup () {
-  createCanvas(windowWidth, windowHeight);
-  bgcolor = color(200);
- // createButton("Change Background");
-}
-
-function mousePressed () {
-  //if touches[x, y, rect_widht, rect_height]
-  if (mouseX >= x && mouseX <= x + rect_widht && mouseY >= x && mouseY <= x + rect_height)
-  bgcolor = color(random(0, 170), random(0, 170), random(0, 170));
-}
-
-function draw () {
-  background(bgcolor);
-  fill(255, 0, 0);
-  rect(x, y, rect_widht, rect_height);
-  fill(255);
-  textSize(60);
-  text('Click on background of canvas.', 100, 100);
-}
-
-/*
 
   var rect_widht = 60;
   var rect_height = 10;
@@ -34,7 +5,6 @@ function draw () {
 
 function setup() {
       createCanvas(windowWidth, windowHeight);
-
 }
 
 function draw() {
@@ -93,10 +63,38 @@ function draw() {
 }
 
 
- function touchStarted () {
- //     function mouseIsPressed () {
+  function mousePressed () {
     if (mouseX >= ((width/8)*2) && mouseX <= ((width/8)*2) + rect_widht && mouseY >= ((width/8)*2) && mouseY <= ((width/8)*2) + rect_height)
     background(0);
+}
+
+/*
+var bgcolor;
+
+  var x = 100;
+  var y = 100;
+  var rect_widht = 200;
+  var rect_height = 150;
+  
+function setup () {
+  createCanvas(windowWidth, windowHeight);
+  bgcolor = color(200);
+ // createButton("Change Background");
+}
+
+function mousePressed () {
+  //if touches[x, y, rect_widht, rect_height]
+  if (mouseX >= x && mouseX <= x + rect_widht && mouseY >= x && mouseY <= x + rect_height)
+  bgcolor = color(random(0, 170), random(0, 170), random(0, 170));
+}
+
+function draw () {
+  background(bgcolor);
+  fill(255, 0, 0);
+  rect(x, y, rect_widht, rect_height);
+  fill(255);
+  textSize(60);
+  text('Click on background of canvas.', 100, 100);
 }
 
 */
