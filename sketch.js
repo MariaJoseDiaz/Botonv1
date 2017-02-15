c4065f3007388aa72bb75bfd3782ebc2c3cbc280
MariaJoseDiaz <mjdiaz9@uc.cl>
2017-02-15 13:10:25 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,36 @@
 
-  var rect_widht = 60;
+ var bgcolor;
+
+  var x = 100;
+  var y = 100;
+  var rect_widht = 200;
+  var rect_height = 150;
+  
+function setup () {
+  createCanvas(windowWidth, windowHeight);
+  bgcolor = color(200);
+ // createButton("Change Background");
+}
+
+function mousePressed () {
+  //if touches[x, y, rect_widht, rect_height]
+  if (mouseX >= x && mouseX <= x + rect_widht && mouseY >= x && mouseY <= x + rect_height)
+  bgcolor = color(random(0, 170), random(0, 170), random(0, 170));
+}
+
+function draw () {
+  background(bgcolor);
+  fill(255, 0, 0);
+  rect(x, y, rect_widht, rect_height);
+  fill(255);
+  textSize(60);
+  text('Click on background of canvas.', 100, 100);
+}
+
+*/
+
+/*
+ var rect_widht = 60;
   var rect_height = 10;
   var bgcolor;
 
@@ -81,33 +112,3 @@ function draw() {
 }
 
 
-/*
-var bgcolor;
-
-  var x = 100;
-  var y = 100;
-  var rect_widht = 200;
-  var rect_height = 150;
-  
-function setup () {
-  createCanvas(windowWidth, windowHeight);
-  bgcolor = color(200);
- // createButton("Change Background");
-}
-
-function mousePressed () {
-  //if touches[x, y, rect_widht, rect_height]
-  if (mouseX >= x && mouseX <= x + rect_widht && mouseY >= x && mouseY <= x + rect_height)
-  bgcolor = color(random(0, 170), random(0, 170), random(0, 170));
-}
-
-function draw () {
-  background(bgcolor);
-  fill(255, 0, 0);
-  rect(x, y, rect_widht, rect_height);
-  fill(255);
-  textSize(60);
-  text('Click on background of canvas.', 100, 100);
-}
-
-*/
